ba9a4e8177e88195ebd1f12247df1c595836e525
ayushishr <128039555+ayushishr@users.noreply.github.com>
2023-06-03 21:29:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-let simpleArray = ['one', 2, 'three', true, false, undefined, null];
-let yourArray=[];
-for(var i=0;i<5;i++){
-	yourArray.push(simpleArray.[i]);
-}
+let yourArray = ["Hello", 42, true, "World", false];
